9f1af7d6867dca05f68914ea801ac61ec0c3a281
misodengaku <misodengaku@gmail.com>
2013-05-04 03:19:13 +0900

    first

---
+++ AntiProcess/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AntiProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.interval = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.interval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "設定保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "監視間隔";
+            // 
+            // interval
+            // 
+            this.interval.Location = new System.Drawing.Point(77, 12);
+            this.interval.Name = "interval";
+            this.interval.Size = new System.Drawing.Size(120, 19);
+            this.interval.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ms";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 124);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "追加";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "選択削除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "イメージ名（例：notepad）";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AntiProcess";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 234);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.interval);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "AntiProcess";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.interval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown interval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ AntiProcess/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace AntiProcess
+{
+    public partial class Form1 : Form
+    {
+
+        private int WM_SYSCOMMAND = 0x112;
+        private IntPtr SC_MINIMIZE = (IntPtr)0xF020;
+        List<string> names = new List<string>();
+        Hashtable setting;
+        const string filename = "antiprocess.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)interval.Value;
+            names.Clear();
+            foreach (var item in listBox1.Items)
+                names.Add(item.ToString());
+            setting["Interval"] = timer1.Interval;
+            setting["Names"] = names[0];
+            XmlSerialize(filename, setting);
+            MessageBox.Show("設定を保存しました", "AntiProcess");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var name in names)
+            {
+                System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcessesByName(name);
+                foreach (var p in ps)
+                    p.Kill();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            interval.Value = timer1.Interval;
+            timer1.Start();
+            if (System.IO.File.Exists(filename))
+            {
+                try
+                {
+                    setting = XmlDeserialize(filename);
+                    timer1.Interval = (int)setting["Interval"];
+                    interval.Value = (int)setting["Interval"];
+                    listBox1.Items.Add((string)setting["Names"]);
+                    names.Clear();
+                    foreach (var item in listBox1.Items)
+                        names.Add(item.ToString());
+
+                }catch{
+                    setting = new Hashtable();
+                }
+            }
+            else
+            {
+                setting = new Hashtable();
+                setting["Interval"] = 100;
+                XmlSerialize(filename, setting);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        #region タスクトレイ関連
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            //最小化されたときにフォームを非表示にする
+            if ((m.Msg == WM_SYSCOMMAND) && (m.WParam == SC_MINIMIZE))
+            {
+                this.Hide();
+            }
+            //上記以外はデフォルトの処理をおこなう
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+
+        #endregion
+
+
+        #region XML関連
+        /// <summary>
+        /// DictionaryEntry配列をHashtableに変換する
+        /// </summary>
+        /// <param name="ary">変換するDictionaryEntry配列</param>
+        /// <returns>変換されたHashtable</returns>
+        public static Hashtable ConvertArrayToHashtable(DictionaryEntry[] ary)
+        {
+            Hashtable ht = new Hashtable(ary.Length);
+            foreach (DictionaryEntry de in ary)
+            {
+                ht.Add(de.Key, de.Value);
+            }
+            return ht;
+        }
+
+
+        /// <summary>
+        /// HashtableをDictionaryEntryの配列に変換する
+        /// </summary>
+        /// <param name="ht">変換するHashtable</param>
+        /// <returns>変換されたDictionaryEntry配列</returns>
+        public static DictionaryEntry[] ConvertHashtableToArray(Hashtable ht)
+        {
+            DictionaryEntry[] entries = new DictionaryEntry[ht.Count];
+            int entryIndex = 0;
+            foreach (DictionaryEntry de in ht)
+            {
+                entries[entryIndex] = de;
+                entryIndex++;
+            }
+            return entries;
+        }
+
+        /// <summary>
+        /// HashtableをXMLファイルに保存する
+        /// </summary>
+        /// <param name="fileName">保存先のファイル名</param>
+        /// <param name="ht">保存するHashtable</param>
+        public static void XmlSerialize(string fileName, Hashtable ht)
+        {
+            //シリアル化できる型に変換
+            DictionaryEntry[] obj = ConvertHashtableToArray(ht);
+
+            //XMLファイルに保存
+            XmlSerializer serializer =
+                new XmlSerializer(typeof(DictionaryEntry[]));
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+            serializer.Serialize(fs, obj);
+            fs.Close();
+        }
+
+        /// <summary>
+        /// シリアル化されたXMLファイルからHashtableを復元する
+        /// </summary>
+        /// <param name="fileName">復元するXMLファイル名</param>
+        /// <returns>復元されたHashtable</returns>
+        public static Hashtable XmlDeserialize(string fileName)
+        {
+            //XMLファイルから復元
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            try
+            {
+                XmlSerializer serializer =
+                    new XmlSerializer(typeof(DictionaryEntry[]));
+                DictionaryEntry[] obj = (DictionaryEntry[])serializer.Deserialize(fs);
+                fs.Close();
+
+                //Hashtableに戻す
+                Hashtable ht = ConvertArrayToHashtable(obj);
+                return ht;
+            }
+            catch
+            {
+                throw new Exception("Deserialize error.");
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+        #endregion
+
+    }
+}
